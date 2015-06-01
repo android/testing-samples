c580c1e47bb20f6209d169d3b941b3cda1112fb9
Lars Vogel <Lars.Vogel@vogella.com>
2015-06-01 13:42:46 +0200

    Moves the email regex to the app

--- unittesting/BasicSample/app/src/main/java/com/example/android/testing/unittesting/BasicSample/EmailValidator.java
+++ unittesting/BasicSample/app/src/main/java/com/example/android/testing/unittesting/BasicSample/EmailValidator.java
@@ -26,6 +26,19 @@ import java.util.regex.Pattern;
  */
 public class EmailValidator implements TextWatcher {
 
+    /**
+     * Email validation pattern.
+     */
+    public static final Pattern EMAIL_PATTERN = Pattern.compile(
+            "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                    "\\@" +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                    "(" +
+                    "\\." +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                    ")+"
+    );
+
     private boolean mIsValid = false;
 
     public boolean isValid() {
@@ -36,16 +49,16 @@ public class EmailValidator implements TextWatcher {
      * Validates if the given input is a valid email address.
      *
      * @param emailPattern The {@link Pattern} used to validate the given email.
-     * @param email The email to validate.
+     * @param email        The email to validate.
      * @return {@code true} if the input is a valid email. {@code false} otherwise.
      */
-    public static boolean isValidEmail(Pattern emailPattern, CharSequence email) {
-        return email != null && emailPattern.matcher(email).matches();
+    public static boolean isValidEmail(CharSequence email) {
+        return email != null && EMAIL_PATTERN.matcher(email).matches();
     }
 
     @Override
     final public void afterTextChanged(Editable editableText) {
-        mIsValid = isValidEmail(android.util.Patterns.EMAIL_ADDRESS, editableText);
+        mIsValid = isValidEmail(editableText);
     }
 
     @Override
