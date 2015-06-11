2e5e50a6cda3841600ac719f40fd93b2cbebcb78
Stephan Linzner <slinzner@google.com>
2015-06-11 11:57:28 +0100

    Added Runner/Rules 0.3, Espresso 2.2 JARs
    - Bundled and Unbundled JARs are available
    - Updated Eclipse sample and fixed classpath
    
    Change-Id: I062716b24d9e67780d6cd2142b6118c36f1becf8

--- espresso/BasicSampleBundled/remove_license.sh
+++ espresso/BasicSampleBundled/remove_license.sh
@@ -1,5 +1,4 @@
-zip -d libs/hamcrest-core-1.1.jar LICENSE.txt
-zip -d libs/hamcrest-library-1.1.jar LICENSE.txt
-zip -d libs/hamcrest-integration-1.1.jar LICENSE.txt
-zip -d libs/junit-dep-4.10.jar LICENSE.txt
+zip -d libs/hamcrest-core-1.3.jar LICENSE.txt
+zip -d libs/hamcrest-library-1.3.jar LICENSE.txt
+zip -d libs/hamcrest-integration-1.3.jar LICENSE.txt
 
