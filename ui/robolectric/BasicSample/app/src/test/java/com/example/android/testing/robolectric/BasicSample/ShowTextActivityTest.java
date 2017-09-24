61e7e45d19d424f479b41dc635299254443ea457
fbertault <fbertault@google.com>
2017-09-23 17:16:11 -0700

    Add robolectric basic sample

---
+++ ui/robolectric/BasicSample/app/src/test/java/com/example/android/testing/robolectric/BasicSample/ShowTextActivityTest.java
@@ -0,0 +1,39 @@
+package com.example.android.testing.robolectric.BasicSample;
+
+import android.content.Intent;
+import android.widget.TextView;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
+
+/**
+ * Robolectric tests for {@link ShowTextActivity}.
+ */
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class ShowTextActivityTest {
+
+    public static final String MESSAGE = "Example";
+
+    private ShowTextActivity activity;
+
+    @Before
+    public void setUp() {
+        Intent intent = new Intent(RuntimeEnvironment.application, ShowTextActivity.class);
+        intent.putExtra(ShowTextActivity.KEY_EXTRA_MESSAGE, MESSAGE);
+        activity = Robolectric.buildActivity(ShowTextActivity.class, intent).setup().get();
+    }
+
+    @Test
+    public void displayIntentMessage() {
+        TextView show_text_view = activity.findViewById(R.id.show_text_view);
+        Assert.assertThat(show_text_view.getText().toString(), Matchers.equalTo(MESSAGE));
+    }
+}
