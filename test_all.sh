5fe2a5d68920ba4a20f6121ea0fad15474edfbb3
Brett Chabot <brettchabot@google.com>
2018-09-27 14:59:56 -0700

    standardize on gradle 4.6

--- test_all.sh
+++ test_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e  # Exit immediately if a command exits with a non-zero status.
+#set -e  # Exit immediately if a command exits with a non-zero status.
 
 for p in $(cat projects.conf); do
    echo
