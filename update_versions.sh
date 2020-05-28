4ffc88ec5a6c7234374210cb97064477e6560e99
Brett Chabot <brettchabot@google.com>
2020-05-28 12:44:32 -0700

    Update androidx.test to 1.3.0-rc01 versions.

--- update_versions.sh
+++ update_versions.sh
@@ -9,20 +9,21 @@ set -ex  # Exit immediately if a command exits with a non-zero status.
 #repourl="https:\/\/oss.sonatype.org\/content\/repositories\/orgrobolectric-1216"
 #repourl="http:\/\/localhost:1480"
 buildToolsVersion="29.0.3"
+compileSdkVersion="29"
+targetSdkVersion="29"
 androidxAnnotationVersion="1.1.0"
 androidxCoreVersion="1.2.0"
 androidxRecyclerVersion="1.1.0"
 guavaVersion="29.0-android"
-compileSdkVersion="29"
-targetSdkVersion="29"
-runnerVersion="1.3.0-beta02"
-rulesVersion="1.3.0-beta02"
-coreVersion="1.3.0-beta02"
-extJUnitVersion="1.1.2-beta02"
-extTruthVersion="1.3.0-beta02"
-espressoVersion="3.3.0-beta02"
+truthVersion="1.0.1"
+runnerVersion="1.3.0-rc01"
+rulesVersion="1.3.0-rc01"
+coreVersion="1.3.0-rc01"
+extJUnitVersion="1.1.2-rc01"
+extTruthVersion="1.3.0-rc01"
+espressoVersion="3.3.0-rc01"
 robolectricVersion="4.3.1"
-uiautomatorVersion="2.2.0"
+uiAutomatorVersion="2.2.0"
 
 for p in $(cat projects.conf); do
    echo
@@ -43,9 +44,10 @@ for p in $(cat projects.conf); do
    sed -i "s/androidxCoreVersion = \".*\"/androidxCoreVersion = \"$androidxCoreVersion\"/" build.gradle
    sed -i "s/androidxRecyclerVersion = \".*\"/androidxRecyclerVersion = \"$androidxRecyclerVersion\"/" build.gradle
    sed -i "s/guavaVersion = \".*\"/guavaVersion = \"$guavaVersion\"/" build.gradle
+   sed -i "s/truthVersion = \".*\"/truthVersion = \"$truthVersion\"/" build.gradle
    sed -i "s/compileSdkVersion .*/compileSdkVersion $compileSdkVersion/" app/build.gradle
    sed -i "s/targetSdkVersion .*/targetSdkVersion $targetSdkVersion/" app/build.gradle
-   sed -i "s/uiautomatorVersion = \".*\"/uiautomatorVersion = \"$uiautomatorVersion\"/" build.gradle
+   sed -i "s/uiAutomatorVersion = \".*\"/uiAutomatorVersion = \"$uiAutomatorVersion\"/" build.gradle
    sed -i "s/robolectricVersion = \".*\"/robolectricVersion = \"$robolectricVersion\"/" build.gradle
    if [ ! -z "$repourl" ]
    then
