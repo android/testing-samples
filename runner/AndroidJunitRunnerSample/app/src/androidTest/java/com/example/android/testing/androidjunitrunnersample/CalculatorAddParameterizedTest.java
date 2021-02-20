744b07d3554e71b1112924cac84a5ed4a344ea31
dazza5000 <darran.kelinske@gmail.com>
2021-02-19 20:24:49 -0600

    Update grammar of documentation

--- runner/AndroidJunitRunnerSample/app/src/androidTest/java/com/example/android/testing/androidjunitrunnersample/CalculatorAddParameterizedTest.java
+++ runner/AndroidJunitRunnerSample/app/src/androidTest/java/com/example/android/testing/androidjunitrunnersample/CalculatorAddParameterizedTest.java
@@ -35,7 +35,7 @@ import static org.junit.runners.Parameterized.Parameters;
 /**
  * JUnit4 tests for the calculator's add logic.
  *
- * <p> This test uses a Junit4s Parameterized tests features which uses annotations to pass
+ * <p> This test uses Junit4s Parameterized tests features which uses annotations to pass
  * parameters into a unit test. The way this works is that you have to use the {@link Parameterized}
  * runner to run your tests.
  * </p>
