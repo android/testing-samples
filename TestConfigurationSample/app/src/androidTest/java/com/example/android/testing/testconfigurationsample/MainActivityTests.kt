0c9fdd20660f529a1499628ce2b11e2b09d1ec55
Jolanda Verhoef <JolandaVerhoef@users.noreply.github.com>
2021-09-14 15:17:43 +0100

    [ConfigSample] Initial commit

---
+++ TestConfigurationSample/app/src/androidTest/java/com/example/android/testing/testconfigurationsample/MainActivityTests.kt
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.testing.testconfigurationsample
+
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Assert.assertTrue
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTests {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun sampleTest1() {
+        // Add instrumented tests here
+        runBlocking { delay(10000) }
+        assertTrue(true)
+    }
+
+    @Test
+    fun sampleTest2() {
+        // Add instrumented tests here
+        runBlocking { delay(10000) }
+        assertTrue(true)
+    }
+
+    @Test
+    fun sampleTest3() {
+        // Add instrumented tests here
+        runBlocking { delay(10000) }
+        assertTrue(true)
+    }
+
+    @Test
+    fun sampleTest4() {
+        // Add instrumented tests here
+        runBlocking { delay(10000) }
+        assertTrue(true)
+    }
+
+    /**
+     * When you find an issue with a specific device or API level,
+     * you can create an annotation for it and add it to your test.
+     * In your CI setup you can then run tests with these annotations
+     * on a specific Gradle Managed Virtual Device.
+     *
+     * See the Github Actions setup of this project for an example.
+     */
+    @Test @TestDeviceApi23
+    fun regressionTestKnownIssueApi23() {
+        // Add instrumented tests here
+        runBlocking { delay(10000) }
+        Assert.assertTrue(true)
+    }
+}
