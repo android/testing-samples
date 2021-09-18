d46e0498f7c3ea0bde587139f81d459ebb7fa136
Brett Chabot <brettchabot@google.com>
2021-09-18 12:41:51 -0700

    Update to latest dependencies.
     - swap jcenter for mavenCentral
     - AGP -> 7.0.2
     - build tools -> 31.0.0
     - androidx.annotation -> 1.2.0
     - androidx.core -> 1.6.0
     - androidx.recyclerview -> 1.2.1
     - truth -> 1.1.3
     - guava -> 30.1.1-android

--- update_versions.sh
+++ update_versions.sh
@@ -8,21 +8,25 @@ set -ex  # Exit immediately if a command exits with a non-zero status.
 #repourl="file:\/\/\/~\/test_repos\/1.4.1-alpha01\/m2repository"
 #repourl="https:\/\/oss.sonatype.org\/content\/repositories\/orgrobolectric-1216"
 #repourl="http:\/\/localhost:1480"
-buildToolsVersion="30.0.2"
+buildToolsVersion="31.0.0"
+agpVersion="7.0.2"
+kotlinVersion="1.4.31"
 compileSdkVersion="30"
 targetSdkVersion="30"
-androidxAnnotationVersion="1.1.0"
-androidxCoreVersion="1.2.0"
-androidxRecyclerVersion="1.1.0"
-guavaVersion="29.0-android"
-truthVersion="1.0.1"
+androidxAnnotationVersion="1.2.0"
+androidxCompatVersion="1.3.1"
+androidxCoreVersion="1.6.0"
+androidxFragmentVersion="1.3.6"
+androidxRecyclerVersion="1.2.1"
+guavaVersion="30.1.1-android"
+truthVersion="1.1.3"
 runnerVersion="1.4.1-alpha01"
 rulesVersion="1.4.1-alpha01"
 coreVersion="1.4.1-alpha01"
 extJUnitVersion="1.1.4-alpha01"
 extTruthVersion="1.5.0-alpha01"
 espressoVersion="3.5.0-alpha01"
-robolectricVersion="4.5.1"
+robolectricVersion="4.6.1"
 uiAutomatorVersion="2.2.0"
 
 for p in $(cat projects.conf); do
@@ -33,15 +37,19 @@ for p in $(cat projects.conf); do
    pushd $p > /dev/null  # Silent pushd
 
    # Replace versions
+   sed -i "s/buildToolsVersion = \".*\"/buildToolsVersion = \"$buildToolsVersion\"/" build.gradle
+   sed -i "s/agpVersion = \".*\"/agpVersion = \"$agpVersion\"/" build.gradle
+   sed -i "s/kotlinVersion = \".*\"/kotlinVersion = \"$kotlinVersion\"/" build.gradle
    sed -i "s/coreVersion = \".*\"/coreVersion = \"$coreVersion\"/" build.gradle
    sed -i "s/extJUnitVersion = \".*\"/extJUnitVersion = \"$extJUnitVersion\"/" build.gradle
    sed -i "s/extTruthVersion = \".*\"/extTruthVersion = \"$extTruthVersion\"/" build.gradle
    sed -i "s/runnerVersion = \".*\"/runnerVersion = \"$runnerVersion\"/" build.gradle
    sed -i "s/rulesVersion = \".*\"/rulesVersion = \"$rulesVersion\"/" build.gradle
    sed -i "s/espressoVersion = \".*\"/espressoVersion = \"$espressoVersion\"/" build.gradle
-   sed -i "s/buildToolsVersion = \".*\"/buildToolsVersion = \"$buildToolsVersion\"/" build.gradle
    sed -i "s/androidxAnnotationVersion = \".*\"/androidxAnnotationVersion = \"$androidxAnnotationVersion\"/" build.gradle
+   sed -i "s/androidxCompatVersion = \".*\"/androidxCompatVersion = \"$androidxCompatVersion\"/" build.gradle
    sed -i "s/androidxCoreVersion = \".*\"/androidxCoreVersion = \"$androidxCoreVersion\"/" build.gradle
+   sed -i "s/androidxFragmentVersion = \".*\"/androidxFragmentVersion = \"$androidxFragmentVersion\"/" build.gradle
    sed -i "s/androidxRecyclerVersion = \".*\"/androidxRecyclerVersion = \"$androidxRecyclerVersion\"/" build.gradle
    sed -i "s/guavaVersion = \".*\"/guavaVersion = \"$guavaVersion\"/" build.gradle
    sed -i "s/truthVersion = \".*\"/truthVersion = \"$truthVersion\"/" build.gradle
@@ -51,7 +59,7 @@ for p in $(cat projects.conf); do
    sed -i "s/robolectricVersion = \".*\"/robolectricVersion = \"$robolectricVersion\"/" build.gradle
    if [ ! -z "$repourl" ]
    then
-         sed -i "s/.*jcenter()/        jcenter()\n        maven {\n            url \"$repourl\"\n        }/" build.gradle
+         sed -i "s/.*google()/        google()\n        maven {\n            url \"$repourl\"\n        }/" build.gradle
    fi
    popd > /dev/null  # Silent popd
   done
