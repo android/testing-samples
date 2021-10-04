ef6881ad671424cf6dfda17d3db8faeeb7e2c5da
Brett Chabot <brettchabot@google.com>
2021-10-04 16:53:10 -0700

    reorganize imports

--- ui/espresso/ScreenshotSample/app/src/androidTest/java/com/example/android/testing/espresso/screenshotsample/ScreenshotTest.kt
+++ ui/espresso/ScreenshotSample/app/src/androidTest/java/com/example/android/testing/espresso/screenshotsample/ScreenshotTest.kt
@@ -1,12 +1,8 @@
 package com.example.android.testing.espresso.screenshotsample
 
-
-//import androidx.test.core.view.captureToBitmap
-
 import androidx.test.core.app.takeScreenshot
 import androidx.test.core.graphics.writeToTestStorage
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isRoot
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.espresso.screenshot.captureToBitmap
@@ -20,6 +16,13 @@ import java.io.IOException
 
 /*
  * Illustrates usage of APIs to capture a bitmap from view and saving it to test storage.
+ *
+ * The exact path will vary based on android API version, but the saved files can be retrieved via
+ * Device File Explorer at a path like
+ * /storage/emulated/0/googletest/test_outputfiles
+ *
+ * A future Android Gradle Plugin version should auto-retrieve these files from the device onto the
+ * host.
  */
 @RunWith(AndroidJUnit4::class)
 class ScreenshotTest {
@@ -64,5 +67,3 @@ class ScreenshotTest {
       .writeToTestStorage(nameRule.methodName)
   }
 }
-
-
