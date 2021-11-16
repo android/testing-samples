5f02295836d4878968d319218cc3f9aa7db9fbd8
Jose Alcerreca <jalc@google.com>
2021-11-16 14:40:22 +0100

    Do not merge: test module and GMD repro

---
+++ TestConfigurationSample/nav-tests/src/main/java/com/example/android/testing/navtests/ExampleInstrumentedTest.kt
@@ -0,0 +1,22 @@
+package com.example.android.testing.navtests
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.android.testing.navtests", appContext.packageName)
+    }
+}
