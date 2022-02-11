d1baaf85a82a9b6f060a00230231659e4160bd8b
Raphael Moll <raphael@google.com>
2022-02-11 14:26:13 -0800

    Update versions to handle single-quote versions & settings.gradle.

--- update_versions.sh
+++ update_versions.sh
@@ -8,29 +8,30 @@ set -ex  # Exit immediately if a command exits with a non-zero status.
 #repourl="file:\/\/\/tmp\/test_repo\/m2repository"
 #repourl="https:\/\/oss.sonatype.org\/content\/repositories\/orgrobolectric-1216"
 #repourl="http:\/\/localhost:1480"
+# Versions:                             # axt_versions.bzl equivalents
 buildToolsVersion="31.0.0"
 agpVersion="7.0.2"
-kotlinVersion="1.4.31"
+kotlinVersion="1.4.31"                  # KOTLIN_VERSION
 compileSdkVersion="30"
 targetSdkVersion="30"
-androidxAnnotationVersion="1.2.0"
-androidxCompatVersion="1.3.1"
-androidxCoreVersion="1.6.0"
-androidxFragmentVersion="1.3.6"
-androidxRecyclerVersion="1.2.1"
-guavaVersion="30.1.1-android"
-truthVersion="1.1.3"
-runnerVersion="1.4.1-alpha03"
-monitorVersion="1.5.0"
-rulesVersion="1.4.1-alpha03"
-servicesVersion="1.4.1"
-orchestratorVersion="1.4.1"
-coreVersion="1.4.1-alpha03"
-extJUnitVersion="1.1.4-alpha03"
-extTruthVersion="1.5.0-alpha03"
-espressoVersion="3.5.0-alpha03"
+androidxAnnotationVersion="1.2.0"       # ANDROIDX_ANNOTATION_VERSION
+androidxCompatVersion="1.3.1"           # ANDROIDX_COMPAT_VERSION
+androidxCoreVersion="1.6.0"             # ANDROIDX_CORE_VERSION
+androidxFragmentVersion="1.3.6"         # ANDROIDX_FRAGMENT_VERSION
+androidxRecyclerVersion="1.2.1"         # ANDROIDX_RECYCLERVIEW_VERSION
+guavaVersion="30.1.1-android"           # GUAVA_VERSION
+truthVersion="1.1.3"                    # TRUTH_VERSION
+runnerVersion="1.4.1-alpha03"           # RUNNER_VERSION
+monitorVersion="1.5.0"                  # MONITOR_VERSION
+rulesVersion="1.4.1-alpha03"            # RULES_VERSION
+servicesVersion="1.4.1"                 # SERVICES_VERSION
+orchestratorVersion="1.4.1"             # ORCHESTRATOR_VERSION
+coreVersion="1.4.1-alpha03"             # CORE_VERSION
+extJUnitVersion="1.1.4-alpha03"         # ANDROIDX_JUNIT_VERSION
+extTruthVersion="1.5.0-alpha03"         # ANDROIDX_TRUTH_VERSION
+espressoVersion="3.5.0-alpha03"         # ESPRESSO_VERSION
 robolectricVersion="4.6.1"
-uiAutomatorVersion="2.2.0"
+uiAutomatorVersion="2.2.0"              # UIAUTOMATOR_VERSION
 
 for p in $(cat projects.conf); do
    echo
@@ -40,32 +41,34 @@ for p in $(cat projects.conf); do
    pushd $p > /dev/null  # Silent pushd
 
    # Replace versions
-   sed -i "s/buildToolsVersion = \".*\"/buildToolsVersion = \"$buildToolsVersion\"/" build.gradle
-   sed -i "s/agpVersion = \".*\"/agpVersion = \"$agpVersion\"/" build.gradle
-   sed -i "s/kotlinVersion = \".*\"/kotlinVersion = \"$kotlinVersion\"/" build.gradle
-   sed -i "s/coreVersion = \".*\"/coreVersion = \"$coreVersion\"/" build.gradle
-   sed -i "s/extJUnitVersion = \".*\"/extJUnitVersion = \"$extJUnitVersion\"/" build.gradle
-   sed -i "s/extTruthVersion = \".*\"/extTruthVersion = \"$extTruthVersion\"/" build.gradle
-   sed -i "s/monitorVersion = \".*\"/monitorVersion = \"$monitorVersion\"/" build.gradle
-   sed -i "s/runnerVersion = \".*\"/runnerVersion = \"$runnerVersion\"/" build.gradle
-   sed -i "s/rulesVersion = \".*\"/rulesVersion = \"$rulesVersion\"/" build.gradle
-   sed -i "s/servicesVersion = \".*\"/servicesVersion = \"$servicesVersion\"/" build.gradle
-   sed -i "s/orchestratorVersion = \".*\"/orchestratorVersion = \"$orchestratorVersion\"/" build.gradle
-   sed -i "s/espressoVersion = \".*\"/espressoVersion = \"$espressoVersion\"/" build.gradle
-   sed -i "s/androidxAnnotationVersion = \".*\"/androidxAnnotationVersion = \"$androidxAnnotationVersion\"/" build.gradle
-   sed -i "s/androidxCompatVersion = \".*\"/androidxCompatVersion = \"$androidxCompatVersion\"/" build.gradle
-   sed -i "s/androidxCoreVersion = \".*\"/androidxCoreVersion = \"$androidxCoreVersion\"/" build.gradle
-   sed -i "s/androidxFragmentVersion = \".*\"/androidxFragmentVersion = \"$androidxFragmentVersion\"/" build.gradle
-   sed -i "s/androidxRecyclerVersion = \".*\"/androidxRecyclerVersion = \"$androidxRecyclerVersion\"/" build.gradle
-   sed -i "s/guavaVersion = \".*\"/guavaVersion = \"$guavaVersion\"/" build.gradle
-   sed -i "s/truthVersion = \".*\"/truthVersion = \"$truthVersion\"/" build.gradle
+   sed -i "s/buildToolsVersion = \([\"']\).*\1/buildToolsVersion = \"$buildToolsVersion\"/" build.gradle
+   sed -i "s/agpVersion = \([\"']\).*\1/agpVersion = \"$agpVersion\"/" build.gradle
+   sed -i "s/kotlinVersion = \([\"']\).*\1/kotlinVersion = \"$kotlinVersion\"/" build.gradle
+   sed -i "s/coreVersion = \([\"']\).*\1/coreVersion = \"$coreVersion\"/" build.gradle
+   sed -i "s/extJUnitVersion = \([\"']\).*\1/extJUnitVersion = \"$extJUnitVersion\"/" build.gradle
+   sed -i "s/extTruthVersion = \([\"']\).*\1/extTruthVersion = \"$extTruthVersion\"/" build.gradle
+   sed -i "s/monitorVersion = \([\"']\).*\1/monitorVersion = \"$monitorVersion\"/" build.gradle
+   sed -i "s/runnerVersion = \([\"']\).*\1/runnerVersion = \"$runnerVersion\"/" build.gradle
+   sed -i "s/rulesVersion = \([\"']\).*\1/rulesVersion = \"$rulesVersion\"/" build.gradle
+   sed -i "s/servicesVersion = \([\"']\).*\1/servicesVersion = \"$servicesVersion\"/" build.gradle
+   sed -i "s/orchestratorVersion = \([\"']\).*\1/orchestratorVersion = \"$orchestratorVersion\"/" build.gradle
+   sed -i "s/espressoVersion = \([\"']\).*\1/espressoVersion = \"$espressoVersion\"/" build.gradle
+   sed -i "s/androidxAnnotationVersion = \([\"']\).*\1/androidxAnnotationVersion = \"$androidxAnnotationVersion\"/" build.gradle
+   sed -i "s/androidxCompatVersion = \([\"']\).*\1/androidxCompatVersion = \"$androidxCompatVersion\"/" build.gradle
+   sed -i "s/androidxCoreVersion = \([\"']\).*\1/androidxCoreVersion = \"$androidxCoreVersion\"/" build.gradle
+   sed -i "s/androidxFragmentVersion = \([\"']\).*\1/androidxFragmentVersion = \"$androidxFragmentVersion\"/" build.gradle
+   sed -i "s/androidxRecyclerVersion = \([\"']\).*\1/androidxRecyclerVersion = \"$androidxRecyclerVersion\"/" build.gradle
+   sed -i "s/guavaVersion = \([\"']\).*\1/guavaVersion = \"$guavaVersion\"/" build.gradle
+   sed -i "s/truthVersion = \([\"']\).*\1/truthVersion = \"$truthVersion\"/" build.gradle
    sed -i "s/compileSdkVersion .*/compileSdkVersion $compileSdkVersion/" app/build.gradle
    sed -i "s/targetSdkVersion .*/targetSdkVersion $targetSdkVersion/" app/build.gradle
-   sed -i "s/uiAutomatorVersion = \".*\"/uiAutomatorVersion = \"$uiAutomatorVersion\"/" build.gradle
-   sed -i "s/robolectricVersion = \".*\"/robolectricVersion = \"$robolectricVersion\"/" build.gradle
+   sed -i "s/uiAutomatorVersion = \([\"']\).*\1/uiAutomatorVersion = \"$uiAutomatorVersion\"/" build.gradle
+   sed -i "s/robolectricVersion = \([\"']\).*\1/robolectricVersion = \"$robolectricVersion\"/" build.gradle
    if [ ! -z "$repourl" ]
    then
-         sed -i "s/.*google()/        google()\n        maven {\n            url \"$repourl\"\n          allowInsecureProtocol=true\n        }/" build.gradle
+      for G in build.gradle settings.gradle; do
+          sed -i "s/.*google()/        google()\n        maven {\n            url \"$repourl\"\n          allowInsecureProtocol=true\n        }/" $G
+      done
    fi
    popd > /dev/null  # Silent popd
   done
