998835bcb281c3eceabb75bcae3225a1152a6ad9
Fred Sladkey <fsladkey@google.com>
2022-05-24 10:35:44 -0400

    Move project to unit/ directory and make updates based on feedback

--- unit/BasicNativeAndroidTest/app/src/androidTest/java/com/example/android/testing/nativesample/AdderTest.kt
+++ unit/BasicNativeAndroidTest/app/src/androidTest/java/com/example/android/testing/nativesample/AdderTest.kt
@@ -4,6 +4,9 @@ import androidx.test.ext.junitgtest.GtestRunner
 import androidx.test.ext.junitgtest.TargetLibrary
 import org.junit.runner.RunWith
 
+
+// Run our tests with the `GtestRunner`, which knows how to load and run GTest suites.
 @RunWith(GtestRunner::class)
+// Specify the name of the artifact which contains our tests, as configured in CMakeLists.txt
 @TargetLibrary(libraryName = "adder-test")
 class AdderTest
