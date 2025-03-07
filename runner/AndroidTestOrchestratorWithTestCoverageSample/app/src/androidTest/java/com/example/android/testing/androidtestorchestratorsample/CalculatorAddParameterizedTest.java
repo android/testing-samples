9a20ab25b4658f5f8e05698dca33373f77666e5d
vijayvamsi279 <gummadivamsi1996@gmail.com>
2025-03-07 18:01:57 +0530

    Update CalculatorAddParameterizedTest.java

--- runner/AndroidTestOrchestratorWithTestCoverageSample/app/src/androidTest/java/com/example/android/testing/androidtestorchestratorsample/CalculatorAddParameterizedTest.java
+++ runner/AndroidTestOrchestratorWithTestCoverageSample/app/src/androidTest/java/com/example/android/testing/androidtestorchestratorsample/CalculatorAddParameterizedTest.java
@@ -18,7 +18,8 @@ package com.example.android.testing.androidtestorchestratorsample;
 
 import static com.google.common.truth.Truth.assertThat;
 
-import org.junit.Before;
+
+import org.junit. chrome Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
